82692deab40bfaa629a3382defae94be0171ab70
Budy Santoso <153147979+budy54nt050@users.noreply.github.com>
2024-01-10 09:49:07 +0700

    Variabel data

---
+++ main.py
@@ -0,0 +1,31 @@
+#Ini adalah cooment pertama
+print("Hello World!!!")
+
+# Menaruh/assignment nilai
+a = 10
+x = 25
+panjang = 30
+print(a)
+pj = panjang + a + x
+
+
+#pemanggilan pertama
+print("Nilai a = ", a) #Ini adalah nilai a
+print("Nilai x = ", x) #Ini adalah nilai x
+print("Nilai panjang = ", panjang) #Ini adalah nilai panjang
+print("Nilai pj = ", pj) #Ini adalah nilai pj
+
+# penamaan
+nilai_y = 7 # tidak boleh ada spasi, boleh dengan underscore
+juta15 = 15000000 # tidak boleh didahului dengan angka, harus didahului dengan huruf
+
+#pemanggilan kedua
+print("Nilai pj = ", pj) #Ini adalah nilai pj
+a = 12
+print("Nilai a = ", a) #Ini adalah nilai a yang baru
+
+#indirect assignment
+b = a
+print("Nilai b = ", b) #Ini adalah nilai b
+
+
